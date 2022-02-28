1cea3740307d3f82d3cce5999754caf7b60c7c2d
Michael Clermont <michael.clermont@mths.ca>
2022-02-28 22:38:15 +0000

    added area and perimeter program

---
+++ areaAndperimeter.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2022 Michael Clermont All rights reserved
+//
+// Created by: Michael Clermont
+// Created on: Feb 2022
+// This program calculates the area and perimeter of a rectangle
+//    with radius inputted by the user
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    // this function calculates the area and perimeter inputted by user
+    int length;
+    int width;
+    int area;
+    int perimeter;
+
+
+    // input
+    std::cout << "Enter length of the rectangle (mm): ";
+    std::cin >> length;
+    std::cout << "Enter width of the rectangle (mm): ";
+    std::cin >> width;
+
+
+    // process
+    area = length * width;
+    perimeter = 2 *(length + width);
+
+
+    // output
+    std::cout << "" << std:: endl;
+    std::cout << "Area is "<< area << "mm²." << std::endl;
+    std::cout << "Perimeter is "<< perimeter << "mm." << std::endl;
+    std::cout<< "\nDone." << std::endl;
+}
